ce0c91f2b089feaa6a9fefe0e3ffa5b2e871b74f
Jeff Remillard <jeffrey.remillard@verizonconnect.com>
2019-01-08 18:53:48 -0500

    added subject and creator methods

--- src/main/java/net/remgant/library/LibraryController.java
+++ src/main/java/net/remgant/library/LibraryController.java
@@ -24,11 +24,25 @@ public class LibraryController {
         listBuilder.add(ImmutableMap.of("type","subject","topic","England -- Geography","url","subject/2","count",3));
         listBuilder.add(ImmutableMap.of("type","subject","topic","England -- Fiction","url","subject/3","count",5));
         listBuilder.add(ImmutableMap.of("type","creator","topic","England, Joseph","url","creator/6","count",1));
-
-//        builder.put("results",listBuilder.build());
-//        return builder.build();
         return listBuilder.build();
     }
 
 
+    @RequestMapping(value="/subject/{subjectId}", method = RequestMethod.GET)
+    @ResponseBody List<Map<String,Object>> subject(@PathVariable("subjectId") int subjectId) {
+        log.info("got subject request: {}",subjectId);
+        ImmutableList.Builder<Map<String,Object>> listBuilder = ImmutableList.builder();
+        listBuilder.add(ImmutableMap.of("title","Wind in the Willows, The", "author","Grahame, Kenneth","url","book/22"));
+        listBuilder.add(ImmutableMap.of("title","Persuasion", "author","Austen, Jane","url","book/44"));
+        return listBuilder.build();
+    }
+
+    @RequestMapping(value="/creator/{creatorId}", method = RequestMethod.GET)
+    @ResponseBody List<Map<String,Object>> creator(@PathVariable("creatorId") int creatorId) {
+        log.info("got creator request: {}",creatorId);
+        ImmutableList.Builder<Map<String,Object>> listBuilder = ImmutableList.builder();
+        listBuilder.add(ImmutableMap.of("title","Wind in the Willows, The", "author","Grahame, Kenneth","url","book/22"));
+        listBuilder.add(ImmutableMap.of("title","Persuasion", "author","Austen, Jane","url","book/44"));
+        return listBuilder.build();
+    }
 }
